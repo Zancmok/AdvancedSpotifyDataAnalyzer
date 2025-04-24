a3309f66159579e98a54fbbd476a0cca8dcf5b13
Zancmok <nejc.germovsek@gmail.com>
2025-04-24 15:24:09 +0200

    Added docker-compose.yaml.

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,12 @@ FROM python:3.13-slim
 WORKDIR /app
 
 COPY ./src /app/src
-
 COPY requirements.txt /app/
 
-# COPY .env /app/
+# Create the database folder and file in a single RUN statement
+RUN mkdir -p /app/database && touch /app/database/database.db
 
+# Install dependencies
 RUN pip install --no-cache-dir -r /app/requirements.txt
 
 EXPOSE 5000
--- src/SpotifyAnalyzer/config.py
+++ src/SpotifyAnalyzer/config.py
@@ -1,8 +1,11 @@
 DEBUG: bool = True
 PORT: int = 5000
 HOST: str = "0.0.0.0"
-STATIC_PATH: str = "../static"
-TEMPLATES_PATH: str = "../templates"
-SQL_PATH: str = "./src/sql"
+# STATIC_PATH: str = "../static"
+STATIC_PATH: str = "/app/src/static"
+# TEMPLATES_PATH: str = "../templates"
+TEMPLATES_PATH: str = "/app/src/templates"
+# SQL_PATH: str = "./src/sql"
+SQL_PATH: str = "/app/src/sql"
 FLASK_SECRET_KEY: str = "mikudayo"
-DATABASE_PATH: str = "database.db"
+DATABASE_PATH: str = "/app/database/database.db"
