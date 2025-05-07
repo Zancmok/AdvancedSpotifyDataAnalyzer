206119d6ea3e1c11fe65560bd26a79ba61d8358c
immanis70 <rozle.lipej@gmail.com>
2025-05-07 07:27:43 +0000

    zip upload working almost

--- src/static/js/settings.js
+++ src/static/js/settings.js
@@ -16,3 +16,32 @@ function comparePasswords() {
 
 newPassword.addEventListener("input", comparePasswords);
 confirmNewPassword.addEventListener("input", comparePasswords);
+
+function uploadZip() {
+	const fileInput = document.getElementById('zipFile');
+	const formData = new FormData();
+
+	if (fileInput.files.length === 0) {
+		alert("Please select a ZIP file.");
+		return;
+	}
+
+	formData.append("file", fileInput.files[0]);
+
+	fetch(zipUploadUrl, {
+		method: 'POST',
+		body: formData
+	})
+	.then(response => response.json())
+	.then(data => {
+		if (data.success) {
+			alert('File uploaded successfully!');
+		} else {
+			alert('Error uploading file.');
+		}
+	})
+	.catch(error => {
+		console.error('Error:', error);
+		alert('An error occurred while uploading the file.');
+	});
+}
