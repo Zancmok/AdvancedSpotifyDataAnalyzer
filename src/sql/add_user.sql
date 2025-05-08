85bd5bf0d1eb96ecc7cfea729693b76cdff45410
immanis70 <rozle.lipej@gmail.com>
2025-05-08 09:27:02 +0000

    sql stuff

--- src/sql/add_user.sql
+++ src/sql/add_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO User (username, password)
-VALUES (:username, :password);
+INSERT INTO User (username, password, profile_picture, last_time_uploaded)
+VALUES (:username, :password, :profile_picture, datetime('now'));
