651d299cd0731821fad37b41aa4b0490fd4eb29d
Zancmok <nejc.germovsek@gmail.com>
2025-05-20 16:06:01 +0200

    Mini fix.

--- src/sql/song_page/get_user_listen_times.sql
+++ src/sql/song_page/get_user_listen_times.sql
@@ -8,12 +8,6 @@ JOIN
     SongListen sl ON u.id = sl.user_id
 JOIN
     Song s ON sl.song_id = s.id
-JOIN
-    Album a ON s.album_id = a.id
-JOIN
-    Author au ON a.author_id = au.id
-JOIN
-    AuthorGenre ag ON au.id = ag.author_id
 WHERE
     sl.timestamp >= :start_date
     AND sl.timestamp < :end_date
